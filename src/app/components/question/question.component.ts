0ea0c7aa83e5d142a2911576d2b2b6114b01fc0a
Adnan <MAdnan@jetblue.com>
2017-08-24 13:08:58 -0400

    added question component as a child component to question list

---
+++ src/app/components/question/question.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from "@angular/core";
+import { Question } from "../../models/question";
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent implements OnInit {
+
+  @Input('question') 
+  question:Question;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
