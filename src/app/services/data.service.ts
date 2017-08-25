ff6efec1d64b03a53d5f18ef5c2b03590fc49ce6
Adnan <MAdnan@jetblue.com>
2017-08-25 16:21:55 -0400

    fixed reload issue + added local storage capability

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,37 +5,64 @@ import { Question } from "../models/question";
 export class DataService {
   questions: Question[];
   constructor() { 
-    this.questions = [
-      {
-        text: 'What is your name?',
-        answer: 'Brad',
-        hide: true
-      },
-      {
-        text: 'What is your fav color?',
-        answer: 'Black',
-        hide: true
-      },
-      {
-        text: 'What is your afv language?',
-        answer: 'Javascript',
-        hide: true
-      }
-    ];
+    // this.questions = [
+    //   {
+    //     text: 'What is your name?',
+    //     answer: 'Brad',
+    //     hide: true
+    //   },
+    //   {
+    //     text: 'What is your fav color?',
+    //     answer: 'Black',
+    //     hide: true
+    //   },
+    //   {
+    //     text: 'What is your afv language?',
+    //     answer: 'Javascript',
+    //     hide: true
+    //   }
+    // ];
   }
 
+  // get questions from locall storage
   getQuestions(){
+    if (localStorage.getItem('questions') === null) {
+      this.questions = [];
+    } else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
     return this.questions;
   }
 
+  // add qustion to local storage
   addQuestion(question:Question){
     this.questions.unshift(question);
+
+    let questions;
+
+    if (localStorage.getItem('questions') === null) {
+      questions = [];
+      //push new question
+      questions.unshift(question);
+      //set new array to localstorage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    } else {
+      questions = JSON.parse(localStorage.getItem('questions'));
+      // add new question
+      questions.unshift(question);
+      //re set local storage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }
+
   }
 
+  //remove individual question from local storage
   removeQuestion(question:Question){
     for (let index = 0; index < this.questions.length; index++) {
       if (question == this.questions[index]) {
         this.questions.splice(index,1);
+        localStorage.setItem('questions', JSON.stringify(this.questions));
+
       }
       
     }
